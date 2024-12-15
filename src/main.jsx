1a49be7d449e70848bc806b274c88e37e077ad63
azzansm <azzanasimaa@gmail.com>
2024-12-15 23:32:51 +0700

    updating

--- src/main.jsx
+++ src/main.jsx
@@ -9,31 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// // Main.jsx
-// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// import App from './App'; // Home component
-// import PredictionInsight from './utils/PredictionInsight';
-// // import SentimentAnalysis from './SentimentAnalysis';
-
-// const Main = () => {
-//   return (
-//     <BrowserRouter>
-//       <header className="toolbar">
-//         <nav>
-//           <Link to="/">Home</Link>
-//           <Link to="/prediction-insight">Prediction Insights</Link>
-//           {/* <Link to="/sentiment-analysis">Sentiment Analysis</Link> */}
-//         </nav>
-//       </header>
-
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//         <Route path="/prediction-insight" element={<PredictionInsight />} />
-//         {/* <Route path="/sentiment-analysis" element={<SentimentAnalysis />} /> */}
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default Main;
